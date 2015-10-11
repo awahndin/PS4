eb12d479d678c1283311745afdcc122c4d9d0396
Awah Ndingwan <Awahndin@udel.edu>
2015-10-10 20:30:23 -0400

    Upload

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -5,14 +5,18 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.util.List;
 import java.util.Map;
 
 import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonEditDialogController;
+import ch.makery.address.view.PersonOverviewController;
 
 import java.util.Collections;
 import javafx.collections.ObservableList;
@@ -35,7 +39,7 @@ public class MainApp extends Application {
     /**
      * Constructor
      */
-    
+
     public MainApp() {
         // Add some sample data
         personData.add(new Person("Hans", "Muster"));
@@ -63,6 +67,7 @@ public class MainApp extends Application {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
 
+        this.primaryStage.getIcons().add(new Image("file:resources/images/book.png"));
         initRootLayout();
 
         showPersonOverview();
@@ -99,6 +104,11 @@ public class MainApp extends Application {
 
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -111,7 +121,44 @@ public class MainApp extends Application {
     public Stage getPrimaryStage() {
         return primaryStage;
     }
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     * 
+     * @param person the person object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
 
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
